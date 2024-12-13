0b6f5fe07ee79271553f5bf434ce2de5ce94545d
EsileGit <esile.comm@gmail.com>
2024-12-13 11:09:27 +0100

    [Game] Added player controller and player state classes

---
+++ ChristmasReigns/Source/ChristmasReigns/Player/CRPlayerController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CRPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHRISTMASREIGNS_API ACRPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
---
+++ ChristmasReigns/Source/ChristmasReigns/Player/CRPlayerState.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "CRPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHRISTMASREIGNS_API ACRPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+};
