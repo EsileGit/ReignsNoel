09c4168dd8c3c787e83274dad9e624075245654b
EsileGit <esile.comm@gmail.com>
2024-12-24 13:18:02 +0100

    Fixed delegate types

---
+++ ChristmasReigns/Source/ChristmasReigns/Game/Tool/DelegateTypes.cpp
@@ -0,0 +1,2 @@
+
+#include "DelegateTypes.h"
--- ChristmasReigns/Source/ChristmasReigns/Game/Tool/DelegateTypes.h
+++ ChristmasReigns/Source/ChristmasReigns/Game/Tool/DelegateTypes.h
@@ -2,6 +2,18 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "DelegateTypes.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttributeModifiedSignature, float, NewValue);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnIntValueModifiedSignature, int32, NewValue);
+// Do no touch any of the following code, something makes it magically works, I don't know what
+/**
+ * toto
+
+UCLASS()
+class MEMENTOMORI_API UAttributeDelegateTypes: public UObject
+{
+	GENERATED_BODY()
+public:
+	UAttributeDelegateTypes(){}
+}; */
